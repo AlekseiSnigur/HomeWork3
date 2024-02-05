2d7c39750838702f73b5c67b3d79c63a80ee4be0
Aleksei <alexsnigur8@gmail.com>
2024-02-05 15:13:31 +0200

    Changed increment and decrement methods to void

--- src/NumberManipulator.java
+++ src/NumberManipulator.java
@@ -1,14 +1,12 @@
 public class NumberManipulator {
-        public int incrementByOne(){
+        public void incrementByOne(){
             int num1 = 1;
             num1++;
             System.out.println("Number after increment is: " + num1);
-            return num1;
         }
-        public int decrementByOne(){
+        public void decrementByOne(){
             int num2 = 2;
             num2--;
             System.out.println("Number after decrement is: " + num2);
-            return num2;
         }
 }
